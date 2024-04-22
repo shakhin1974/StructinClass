57c788d1517dd467c5f817563a55663f43d6c307
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-04-22 14:43:40 +0300

    StructinClass.cpp

--- StructinClass/StructinClass.cpp
+++ StructinClass/StructinClass.cpp
@@ -1,44 +1,56 @@
-﻿#include <iostream>
-#include <string>
-#include<windows.h>
+#include <iostream>
+#include<string>
+#include <windows.h>
 using namespace std;
 class Pyear
 {
 public:
-struct Uyear 
-{ 	
-	int day;  
-	int month; 	
-	int year; 
-} st;
-	Pyear(int , int,  int  ); 
-void Vichisli()
-{ 
-	cout<<"Осталось дней = "<<30 - dPost - st.day << " месяцев = " 
-	<< (mPost - st.month) -1  <<"  До окончания  колледжа " <<4 - (st.year - yPost)   <<endl; 	
-}
+	struct Uyear
+	{
+		int day;
+		int month;
+		int year;
+	} st;
+	Pyear(int, int, int);
+	void Vichisli()
+	{
+		cout << "Осталось дней = " << 30 - dPost - st.day << " месяцев = "
+			<< (mPost - st.month) - 1 << "  До окончания  колледжа " << 4 - (st.year - yPost) << endl;
+	}
+		void Kurs() 
+	{
+			if (st.month >= 9)
+			{
+				cout << " \nВы на "  <<(st.year - yPost) + 1 <<"  курсе " << endl;
+			}
+			else
+			{
+				cout << " \nВы на " << (st.year - yPost)  << "  курсе " << endl;
+			}
 
-private: 
-int dPost; 
-int mPost; 
-int yPost; 
+	}
+
+private:
+	int dPost;
+	int mPost;
+	int yPost;
 
 };
-Pyear::Pyear(int dPost, int mPost, int yPost )
-{ 
-	this->dPost = dPost; this->mPost = mPost; this -> yPost = yPost;
-	this->st.day = st.day; this->st.month = st.month; this->st.year = st.year; 
+Pyear::Pyear(int dPost, int mPost, int yPost)
+{
+	this->dPost = dPost; this->mPost = mPost; this->yPost = yPost;
+	this->st.day = st.day; this->st.month = st.month; this->st.year = st.year;
 }
- int main()
-{ 	SetConsoleCP(1251);
+int main()
+{
+	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	Pyear OIBAS1222(1, 9, 2022 );
+	Pyear OIBAS1222(1, 9, 2022);
 	SYSTEMTIME now;
 	GetSystemTime(&now);
 	OIBAS1222.st.day = now.wDay;
 	OIBAS1222.st.month = now.wMonth;
 	OIBAS1222.st.year = now.wYear;
 	cout << OIBAS1222.st.day << " " << OIBAS1222.st.month << " " << OIBAS1222.st.year << endl;
-	OIBAS1222.Vichisli();
- }
- 
+	OIBAS1222.Kurs();
+}
