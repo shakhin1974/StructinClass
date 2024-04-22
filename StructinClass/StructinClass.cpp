ca205f9651816d0f193221b7abeb37f07183a56b
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-04-22 09:44:51 +0300

    StructinClass.cpp

--- StructinClass/StructinClass.cpp
+++ StructinClass/StructinClass.cpp
@@ -2,43 +2,20 @@
 #include <string>
 #include<windows.h>
 using namespace std;
-class Byear
-{
-public:
-	struct Uyear
-	{
-	int day;
-	int month;
-	int year;
-	} st;
-	Byear(int , int,  int  );
-	~Byear();
-	void Vichisli() 
-	{
-		cout<<"Осталось дней = "<<30 - dPost - st.day << " месяцев = " << (mPost - st.month) -1  <<"  До окончания  колледжа " 
-		<<4 - (st.year - yPost)   <<endl;
-	}
-private:
-	int dPost;
-	int mPost;
-	int yPost;
-};
-Byear::Byear(int dPost, int mPost, int yPost )
-{
-	this->dPost = dPost;
-	this->mPost = mPost;
-	this -> yPost = yPost;
-	this->st.day = st.day;
-	this->st.month = st.month;
-	this->st.year = st.year;
-}
-Byear::~Byear()
-{
-}
+class Pyear
+{public:
+	struct Uyear { 	int day;  int month; 	int year; } st;
+	Pyear(int , int,  int  ); 
+void Vichisli()  { cout<<"Осталось дней = "<<30 - dPost - st.day << " месяцев = " 
+	<< (mPost - st.month) -1  <<"  До окончания  колледжа " <<4 - (st.year - yPost)   <<endl; 	}
+private: int dPost; int mPost; int yPost; };
+Pyear::Pyear(int dPost, int mPost, int yPost )
+{ this->dPost = dPost; this->mPost = mPost; this -> yPost = yPost;
+	this->st.day = st.day; this->st.month = st.month; this->st.year = st.year; }
  int main()
 { 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	Byear B1(1, 9, 2022 );
+	Pyear B1(1, 9, 2022 );
 	SYSTEMTIME now;
 	GetSystemTime(&now);
 	int a = B1.st.day = now.wDay;
